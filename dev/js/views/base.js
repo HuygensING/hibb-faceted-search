7f25e223ccc4498d51db11c098a1dc79262d7f93
Gijsjan <agijsbro@gmail.com>
2013-07-08 16:38:45 +0200

    restored base view

--- dev/js/views/base.js
+++ dev/js/views/base.js
@@ -3,17 +3,22 @@
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
   define(function(require) {
-    var Backbone, Base, _ref;
+    var Backbone, BaseView, Pubsub, _ref;
     Backbone = require('backbone');
-    return Base = (function(_super) {
-      __extends(Base, _super);
+    Pubsub = require('managers/pubsub');
+    return BaseView = (function(_super) {
+      __extends(BaseView, _super);
 
-      function Base() {
-        _ref = Base.__super__.constructor.apply(this, arguments);
+      function BaseView() {
+        _ref = BaseView.__super__.constructor.apply(this, arguments);
         return _ref;
       }
 
-      return Base;
+      BaseView.prototype.initialize = function() {
+        return _.extend(this, Pubsub);
+      };
+
+      return BaseView;
 
     })(Backbone.View);
   });
